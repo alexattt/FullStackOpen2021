108491a4eef74230b208630795ab2b9eb76517e5
Aleksandra Timofejeva <aleksandra1338@inbox.lv>
2021-08-01 20:36:00 +0300

    exercises up to 2.14.
    Added weather data for country capitals

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import CountryData from './CountryData'
+import FilterCountries from './FilterCountries'
 import './App.css'
 
 function App() {
@@ -12,11 +12,9 @@ function App() {
     .filter(country => country.name.toLowerCase().includes(filterCountry.toLowerCase()) || filterCountry === '')
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
       })
   }, [])
@@ -29,31 +27,12 @@ function App() {
     setButtonValue(btnValue);
   }
 
-  const onClick = (btnValue) => {
-    handleShowButtonClick(btnValue);
-    setShowCountryData(!showCountryData);
-  }
-
   return (
     <div className="App">
       Find countries: <input value={filterCountry} onChange={handleFilterByCountry}/>
-      {filteredCountries.length > 10 && <p>Please be more specific, too many countries!</p>}
-      {filteredCountries.length < 10 && filteredCountries.length !== 1 && 
-        <ul>
-          {
-            filteredCountries.map(country=> 
-              <div className="countryAndButton" key={country.alpha2Code}>
-                <p> {country.name}</p>
-                <button type="button" onClick={() => onClick(filteredCountries.indexOf(country))}>Show</button>
-              </div>
-            )
-          }
-          {showCountryData ? <CountryData country={filteredCountries[buttonValue]}/> : <div></div>}
-        </ul>
-      }
-      {filteredCountries.length === 1 && 
-        <CountryData country={filteredCountries[0]}/>
-      }
+      <FilterCountries filteredCountries={filteredCountries} showCountryData={showCountryData} 
+                       buttonValue={buttonValue} handleShowButtonClick={handleShowButtonClick} 
+                       setShowCountryData={setShowCountryData} />
     </div>
   );
 }
