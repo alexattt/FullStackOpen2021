2d75a888a32dd9308fe243d42926da9988f5df24
Aleksandra Timofejeva <aleksandra1338@inbox.lv>
2021-10-13 00:57:00 +0300

    learning about react redux connect function

--- part6/notesapp-redux/src/components/NewNote.js
+++ part6/notesapp-redux/src/components/NewNote.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux' 
 import { createNote } from '../reducers/noteReducer'
-import noteService from '../services/notes'
 
-const NewNote = () => {
-  const dispatch = useDispatch()
-
+const NewNote = (props) => {
+  
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    dispatch(createNote(content))
+    props.createNote(content)
   }
 
   return (
@@ -21,4 +19,32 @@ const NewNote = () => {
   )
 }
 
-export default NewNote
+export default connect(
+  null, 
+  { createNote }
+)(NewNote)
+
+// import React from 'react'
+// import { useDispatch } from 'react-redux'
+// import { createNote } from '../reducers/noteReducer'
+// import noteService from '../services/notes'
+
+// const NewNote = () => {
+//   const dispatch = useDispatch()
+
+//   const addNote = async (event) => {
+//     event.preventDefault()
+//     const content = event.target.note.value
+//     event.target.note.value = ''
+//     dispatch(createNote(content))
+//   }
+
+//   return (
+//     <form onSubmit={addNote}>
+//       <input name="note" />
+//       <button type="submit">add</button>
+//     </form>
+//   )
+// }
+
+// export default NewNote
